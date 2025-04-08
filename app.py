320fe282243010bac394a8745d1405715bee124c
LeoGlz-Dev <A110154703@my.uvm.edu.mx>
2025-04-08 11:21:37 -0600

    app.py

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ def home():
     <p>Me gusta: Programar y Jugar Videojuegos </p>
     <p><em>"Cualquier problema tiene un solucion todo depende de la forma en que actues"</em></p>
     """
+
+if __name__ == "__main__":
+    app.run(debug=True)
