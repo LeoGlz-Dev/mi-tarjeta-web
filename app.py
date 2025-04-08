6a558cf58e359eb22d46fe4f2771d1379c2b42a7
LeoGlz-Dev <A110154703@my.uvm.edu.mx>
2025-04-08 11:14:08 -0600

    Tarjeta personal con Flask

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>¡Hola! Soy Leonardo Gonzalez Quijada</h1>
+    <p>Estudio en: Universidad del Valle de Mexico </p>
+    <p>Me gusta: Programar y Jugar Videojuegos </p>
+    <p><em>"Cualquier problema tiene un solucion todo depende de la forma en que actues"</em></p>
+    """
